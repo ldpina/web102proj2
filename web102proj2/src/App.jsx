cff4bff980258fe156e6e455fac80062a0537b1a
ldpina <ldpina@ucsd.edu>
2024-09-30 14:02:35 -0700

    added first steps

--- web102proj2/src/App.jsx
+++ web102proj2/src/App.jsx
@@ -1,34 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="App">
+      <h2>Flash Cards!</h2>
+      <h4>Test your knowledge on!</h4>
+      <h5>Number of Cards: </h5>
+      <br/>
+
+      <div className = "card">
+        <div className = "front">
+          <div className = "back"></div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <button type="next" class="nextCard">⭢</button>
+    </div>
   )
 }
 
